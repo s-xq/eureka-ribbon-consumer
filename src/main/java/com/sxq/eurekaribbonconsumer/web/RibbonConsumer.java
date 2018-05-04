11eeeb72bad7f7f684005fe12b25d8237f75dc7a
s-xq <971070045@qq.com>
2018-05-04 16:19:10 +0800

    添加断路器Hystrix，实现服务调用超时检测(2000ms)以及服务调用失败及时降级处理

--- src/main/java/com/sxq/eurekaribbonconsumer/web/RibbonConsumer.java
+++ src/main/java/com/sxq/eurekaribbonconsumer/web/RibbonConsumer.java
@@ -5,16 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RibbonConsumer {
 
     @Autowired
-    RestTemplate restTemplate;
+    HelloService helloService;
 
     @RequestMapping(value = "ribbon-consumer", method = RequestMethod.GET)
-    public String helloConsumer() {
-        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    public String ribbonConsumer() {
+        return helloService.helloService();
     }
 }
