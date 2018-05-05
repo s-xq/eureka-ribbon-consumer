f712fb4056d75c857d5444d1a6ce463935b602d1
s-xq <971070045@qq.com>
2018-05-05 14:52:03 +0800

    remove hystrix about using cache

--- src/main/java/com/sxq/eurekaribbonconsumer/web/userservice/UserService.java
+++ src/main/java/com/sxq/eurekaribbonconsumer/web/userservice/UserService.java
@@ -1,9 +1,7 @@
 package com.sxq.eurekaribbonconsumer.web.userservice;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.cache.annotation.CacheResult;
 import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
-import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -19,7 +17,6 @@ public class UserService {
     @Autowired
     RestTemplate restTemplate;
 
-    //    @CacheResult(cacheKeyMethod = "getUserCacheKey")
     @HystrixCommand(fallbackMethod = "defaultUser")
     public User getUser(int userId) {
         User user = restTemplate.getForObject("http://HELLO-SERVICE/getUser?userId={1}", User.class, userId);
@@ -28,14 +25,6 @@ public class UserService {
     }
 
 
-    /**
-     * 使用Hystrix缓存需要初始化{@link HystrixRequestContext}，否则无法使用缓存的功能。
-     * 并且{@link HystrixRequestContext}可以有多个，但是彼此之间不共享缓存，调用{@link HystrixRequestContext#shutdown()}将会清除该上下文对应的缓存。
-     * <p>
-     * 使用方法：
-     * https://github.com/Netflix/Hystrix/wiki/How-To-Use#Caching
-     */
-    //    @CacheResult(cacheKeyMethod = "getUserCacheKey")
     @HystrixCommand
     public Future<User> getUserAsync(int userId) {
         return new AsyncResult<User>() {
@@ -50,7 +39,4 @@ public class UserService {
         return new User();
     }
 
-//    public String getUserCacheKey(int userId){
-//        return String.valueOf(userId);
-//    }
 }
