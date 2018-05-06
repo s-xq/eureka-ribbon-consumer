e312034f01229b888c7586f0d413e97526159f77
s-xq <971070045@qq.com>
2018-05-06 11:15:51 +0800

    UserService:support /getUsers interface

--- src/main/java/com/sxq/eurekaribbonconsumer/web/userservice/UserController.java
+++ src/main/java/com/sxq/eurekaribbonconsumer/web/userservice/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -15,14 +16,26 @@ public class UserController {
     UserService userService;
 
     @RequestMapping(value = "getUser", method = RequestMethod.GET)
-    public String getUser() {
+    public User getUser() {
 //        return userService.getUser(new Random().nextInt(100)).toString();
         try {
-            return userService.getUserAsync(new Random().nextInt(100)).get(1000, TimeUnit.MILLISECONDS).toString();
+            return userService.getUserAsync(new Random().nextInt(100)).get(1000, TimeUnit.MILLISECONDS);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return null;
+    }
+
+    @RequestMapping(value = "getUsers", method = RequestMethod.GET)
+    public List<User> getUsers() {
+        Random rand = new Random(17);
+        int[] ids = new int[10];
+        for (int i = 0; i < ids.length; i++) {
+            ids[i] = rand.nextInt(100);
+            System.out.println(String.valueOf(ids[i]));
+        }
+        System.out.println(ids);
+        return userService.getUsers(ids);
     }
 
 
--- src/main/java/com/sxq/eurekaribbonconsumer/web/userservice/UserService.java
+++ src/main/java/com/sxq/eurekaribbonconsumer/web/userservice/UserService.java
@@ -2,10 +2,13 @@ package com.sxq.eurekaribbonconsumer.web.userservice;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.command.AsyncResult;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Future;
 import java.util.logging.Logger;
 
@@ -35,8 +38,28 @@ public class UserService {
         };
     }
 
+    @HystrixCommand(fallbackMethod = "defaultUsers")
+    public List<User> getUsers(int[] userIds) {
+        ArrayList<Integer> userIdList = new ArrayList<>();
+        for (int id : userIds) {
+            userIdList.add(id);
+        }
+        String param = StringUtils.join(userIdList, ",");
+        List<User> users = restTemplate.getForObject("Http://HELLO-SERVICE/getUsers?userIds={1}", List.class, param);
+        mLogger.info(String.format("return %s", users.toString()));
+        return users;
+    }
+
     public User defaultUser(int userId) {
         return new User();
+    }
+
+    public List<User> defaultUsers(int[] userIds) {
+        List<User> res = new ArrayList<>();
+        for (int id : userIds) {
+            res.add(new User());
+        }
+        return res;
     }
 
 }
